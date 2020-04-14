cfd9973a17502cd8c7b569921516a700251c799f
JoonSoo Park <59634896+JoonSoo-Park@users.noreply.github.com>
2020-04-14 23:45:54 +0900

    https://www.acmicpc.net/problem/18870

---
+++ Baekjoon/18870.cpp
@@ -0,0 +1,40 @@
+/*
+* https://www.acmicpc.net/problem/18870
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<pair<int, int> > vec(N);
+    vector<int> answer(N);
+
+    for (int i = 0; i < N; ++i) {
+        cin >> vec[i].first;
+        vec[i].second = i;
+    }
+
+    sort(vec.begin(), vec.end());
+
+    int ct = 0;
+    for (int i = 0; i < N; ++i) {
+        int idx = vec[i].second;
+        int temp = ct;
+
+        answer[idx] = temp;
+        ct++;
+
+        while (i < N && vec[i].first == vec[i + 1].first) {
+            ++i;
+            idx = vec[i].second;
+            answer[idx] = temp;
+        }
+    }
+
+    for (auto a : answer)
+        cout << a << " ";
+
+}
